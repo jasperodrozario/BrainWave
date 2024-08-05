08d3cd1ffcb1bb987ad57c1799bda7b6bc310698
Jasper D Rozario <jasper.odrozario@gmail.com>
2024-08-05 14:15:38 +0600

    Added conversations and more

--- base/models.py
+++ base/models.py
@@ -15,8 +15,8 @@ class Room(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants =
-    updated = models.DateField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -28,8 +28,8 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField(max_length=1000)
-    updated = models.DateField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from .forms import RoomForm
-from .models import Room
+from .models import Room, Topic, Message
 
 # Create your views here.
 
@@ -11,40 +17,100 @@ from .models import Room
 #     {'id':4, 'name':'Backend Developers'},
 # ]
 
+def loginUser(request):
+  page = 'login'
+
+  if request.user.is_authenticated:
+    return redirect('home')
+
+  if request.method == 'POST':
+    username = request.POST.get('username').lower()
+    password = request.POST.get('password')
+    try:
+      user = User.objects.get(username=username)
+    except:
+      messages.error(request, "Username does not exist. Sign up to create a new account.")
+    
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'Incorrect username or password.')
+  context = {'page': page}
+  return render(request, 'base/login_form.html', context)
+
+def logoutUser(request):
+  logout(request)
+  return redirect('home')
+
+def registerUser(request):
+  form = UserCreationForm()
+  context = {'form': form}
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'Something went wrong. Please try again.')
+  return render(request, 'base/login_form.html', context)
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html', context)
+  q = request.GET.get('q') if request.GET.get('q') != None else ''
+  rooms = Room.objects.filter(
+    Q(topic__name__icontains = q) | 
+    Q(name__icontains = q) | 
+    Q(description__icontains = q)
+    )
+  
+  room_count = rooms.count()
+  topics = Topic.objects.all()
+  context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+  return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = Room.objects.get(id=pk)
-    context = {"room": room}
-    return render(request, 'base/room.html', context)
+  room = Room.objects.get(id=pk)
+  if request.method == 'POST':
+    Message.objects.create(
+      user = request.user,
+      room = room,
+      body = request.POST.get('message_box')
+    )
+    return redirect('room', pk=room.id)
+  room_messages = room.message_set.all().order_by('-created')
+  context = {'room': room, 'room_messages': room_messages}
+  return render(request, 'base/room.html', context)
 
+
+@login_required(login_url='login')
 def createRoom(request):
-    form = RoomForm()
-    context = {'form': form}
-    if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid:
-            form.save()
-            return redirect('home')
-    return render(request, 'base/room_form.html', context)
+  form = RoomForm()
+  context = {'form': form}
+  if request.method == 'POST':
+    form = RoomForm(request.POST)
+    if form.is_valid:
+      form.save()
+      return redirect('home')
+  return render(request, 'base/room_form.html', context)
 
 def updateRoom(request, pk):
-    room = Room.objects.get(id=pk)
-    form = RoomForm(instance=room)
-    context = {'form':form}
-    if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid:
-            form.save()
-            return redirect('home')
-    return render(request, 'base/room_form.html', context)
+  room = Room.objects.get(id=pk)
+  form = RoomForm(instance=room)
+  context = {'form':form}
+  if request.method == 'POST':
+    form = RoomForm(request.POST, instance=room)
+    if form.is_valid:
+      form.save()
+      return redirect('home')
+  return render(request, 'base/room_form.html', context)
 
 def deleteRoom(request, pk):
-    room = Room.objects.get(id=pk)
-    if request.method == 'POST':
-        room.delete()
-        return redirect('home')
-    return render(request, 'base/delete.html', {'obj':room})
+  room = Room.objects.get(id=pk)
+  if request.method == 'POST':
+    room.delete()
+    return redirect('home')
+  return render(request, 'base/delete.html', {'obj':room})
