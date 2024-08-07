5088e37dad576e0559ddcc99d8bf2dc32696470a
Jasper D Rozario <jasper.odrozario@gmail.com>
2024-08-07 11:26:33 +0600

    Added Participants Panel

--- base/views.py
+++ base/views.py
@@ -74,15 +74,19 @@ def home(request):
 
 def room(request, pk):
   room = Room.objects.get(id=pk)
+  participants = room.participants.all()
+
   if request.method == 'POST':
     Message.objects.create(
       user = request.user,
       room = room,
       body = request.POST.get('message_box')
     )
+    room.participants.add(request.user)
     return redirect('room', pk=room.id)
+  
   room_messages = room.message_set.all().order_by('-created')
-  context = {'room': room, 'room_messages': room_messages}
+  context = {'room': room, 'room_messages': room_messages, 'participants': participants}
   return render(request, 'base/room.html', context)
 
 
@@ -114,3 +118,10 @@ def deleteRoom(request, pk):
     room.delete()
     return redirect('home')
   return render(request, 'base/delete.html', {'obj':room})
+
+def deleteMessage(request, pk):
+  message = Message.objects.get(id=pk)
+  if request.method == 'POST':
+    message.delete()
+    return redirect('room', pk=message.room.id)
+  return render(request, 'base/delete.html', {'obj': message})
