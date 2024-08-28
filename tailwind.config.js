9b5b2cb890de380e239918ea5843e48a73538ed0
Jasper D Rozario <jasper.odrozario@gmail.com>
2024-08-29 00:30:36 +0600

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html", "./base/templates/base/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
