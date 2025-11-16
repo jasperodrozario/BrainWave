1ea180e29a370872f0990ec9e92f9ee52096a473
Jasper D Rozario <jasper.odrozario@gmail.com>
2025-11-16 12:02:07 +0600

    Rename Project

--- BrainWave/wsgi.py
+++ BrainWave/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DiscordClone project.
+WSGI config for BrainWave project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DiscordClone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrainWave.settings')
 
 application = get_wsgi_application()
